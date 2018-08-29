4f86aa1d49bde9fc05a3b5da3e2247ebe355dd74
”Viktoriia <mizin4ik3@gmail.com>
2018-08-29 10:08:26 +0300

    dynamized group.html

--- students/views.py
+++ students/views.py
@@ -8,23 +8,29 @@ from django.http import HttpResponse
 
 def student_list(request):
     students = (
-        {'id': 1,
-        'first_name': u'Вікторія',
-        'last_name': u'Кіт',
-        'ticket': 235,
-        'image': 'img/pic1.jpg'},
-        {'id': 2,
-        'first_name': u'Софія',
-        'last_name': u'Нельсон',
-        'ticket': 2351,
-        'image': 'img/pic2.jpg'},
-        {'id': 3,
-         'first_name': u'Клава',
-         'last_name': u'Іванишин',
-         'ticket': 329,
-         'image': 'img/pic1.jpg'},
+        {
+            'id': 1,
+            'first_name': u'Вікторія',
+            'last_name': u'Кіт',
+            'ticket': 235,
+            'image': 'img/pic1.jpg'
+        },
+        {
+            'id': 2,
+            'first_name': u'Софія',
+            'last_name': u'Нельсон',
+            'ticket': 2351,
+            'image': 'img/pic2.jpg'
+        },
+        {
+            'id': 3,
+             'first_name': u'Клава',
+             'last_name': u'Іванишин',
+             'ticket': 329,
+             'image': 'img/pic3.jpg'
+        }
     )
-    return render(request, 'students/students_list.html', {'students' :students})
+    return render(request, 'students/students_list.html', {'students' : students})
 
 
 def students_add(request):
@@ -38,19 +44,36 @@ def students_edit(request, sid):
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete student %s</h1>' % sid)
 
-    # Views for Groups
 
+# Views for Groups
 def groups_list(request):
-    return HttpResponse('<h1>Groups listing</h1>')
+    groups = (
+        {
+            'id': 1,
+            'name': u'Мтм-21',
+            'leader': {'id': 1, 'name': u'Кіт Вікторія'}
+        },
+        {
+            'id': 2,
+            'name': u'Мтм-21',
+            'leader': {'id': 2, 'name': u'Нельсон Софія'}
+        },
+        {
+            'id': 3,
+            'name': u'Мтм-21',
+            'leader': {'id': 3, 'name': u'Іванишин Клава'}
+        }
+    )
+    return render(request, 'students/group.html', {'groups': groups})
 
 
 def groups_add(request):
-    return HttpResponse('<h1>Groups  add form</h1>')
+    return HttpResponse('<h1>Group Add Form</h1>')
 
 
 def groups_edit(request, gid):
-    return HttpResponse('<h1>Edit groups %s</h1>' % gid)
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
 
 
 def groups_delete(request, gid):
-    return HttpResponse('<h1>Delete groups %s</h1>' % gid)
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
