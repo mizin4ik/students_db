a944df9c8d0bc1248f4d54a140729b9469fb33ff
”Viktoriia <mizin4ik3@gmail.com>
2018-09-11 15:36:04 +0300

    customized a list of students in the administrative part of Django

--- students/admin.py
+++ students/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
+from django.urls import reverse
+
 from .models import Student, Group, Exam, Result
 
 # Register your models here.
 
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+    list_display_links = ['last_name', 'first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 10
+    search_fields = ['last_name', 'first_name', 'middle_name', 'ticket', 'notes']
+
+    def view_on_site(self, obj):
+        return reverse('students_edit', kwargs = {'pk': obj.id})
+
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Group)
 admin.site.register(Exam)
 admin.site.register(Result)
