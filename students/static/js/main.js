b5c6b4fa7e046aac24b051be995f2bc25d274d43
”Viktoriia <mizin4ik3@gmail.com>
2018-09-21 11:32:18 +0300

    add calendar to edit_students_form

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -51,8 +51,17 @@ function initGroupSelector() {
   });
 }
 
+function initDateFields() {
+    $('input.dateinput').datetimepicker({
+        'format': 'YYYY-MM-DD'
+    }).on('dp.hide', function(event){
+        $(this).blur();
+    });
+}
+
 $(document).ready(function () {
     initJournal();
     initGroupSelector();
+    initDateFields();
     
 });
